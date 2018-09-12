0dd9aeab93ee818d0c3c21e1e17c97dd7c351dfe
CarefreeSlacker <struan.dirk.29@gmail.com>
2018-09-13 00:46:31 +0300

    Fast algorithm has been implemented #sorting-algorithm

--- lib/algorithms/contexts/sorting/brute_force_algorithm.ex
+++ lib/algorithms/contexts/sorting/brute_force_algorithm.ex
@@ -1,7 +1,11 @@
 defmodule Algorithms.Sorting.BruteForseAlgorithm do
-  @moduledoc false
+  @moduledoc """
+  It's intentionally bad algorithm.
+  My purpose is to compare bad and good algorithms.
+  So please don't judge me :D
+  """
 
-  alias Algorithms.Sorting.{Field, Utils}
+  alias Algorithms.Sorting.{Field, Point}
 
   def perform(%Field{field: field}) when length(field) >= 4 do
     length = length(field)
@@ -14,9 +18,9 @@ defmodule Algorithms.Sorting.BruteForseAlgorithm do
         |> Enum.map(fn i3 ->
           (i3 + 1)..(length - 1)
           |> Enum.map(fn i4 ->
-            points = Utils.four_points(field, i1, i2, i3, i4)
+            points = four_points(field, i1, i2, i3, i4)
 
-            if Utils.points_collinear(points) do
+            if points_collinear(points) do
               points |> Enum.sort_by(fn %{x: x} -> x end)
             end
           end)
@@ -32,4 +36,21 @@ defmodule Algorithms.Sorting.BruteForseAlgorithm do
   end
 
   def perform(%Field{field: _field}), do: []
+
+  defp points_collinear([p1, p2, p3, p4]) do
+    slope12 = Point.slope_to(p1, p2)
+    slope13 = Point.slope_to(p1, p3)
+    slope14 = Point.slope_to(p1, p4)
+
+    slope12 == slope13 and slope12 == slope14
+  end
+
+  defp four_points(field, i1, i2, i3, i4) do
+    [
+      Enum.at(field, i1),
+      Enum.at(field, i2),
+      Enum.at(field, i3),
+      Enum.at(field, i4)
+    ]
+  end
 end
--- lib/algorithms/contexts/sorting/utils.ex
+++ lib/algorithms/contexts/sorting/utils.ex
@@ -4,8 +4,6 @@ defmodule Algorithms.Sorting.Utils do
   Helper functions.
   """
 
-  alias Algorithms.Sorting.Point
-
   def closest_fourth_dividable(number, max)
       when is_integer(number) and is_integer(max) and number <= max do
     case Integer.mod(number, 4) do
@@ -16,21 +14,4 @@ defmodule Algorithms.Sorting.Utils do
   end
 
   def closest_fourth_dividable(_number, _max), do: 0
-
-  def points_collinear([p1, p2, p3, p4]) do
-    slope12 = Point.slope_to(p1, p2)
-    slope13 = Point.slope_to(p1, p3)
-    slope14 = Point.slope_to(p1, p4)
-
-    slope12 == slope13 and slope12 == slope14
-  end
-
-  def four_points(field, i1, i2, i3, i4) do
-    [
-      Enum.at(field, i1),
-      Enum.at(field, i2),
-      Enum.at(field, i3),
-      Enum.at(field, i4)
-    ]
-  end
 end
