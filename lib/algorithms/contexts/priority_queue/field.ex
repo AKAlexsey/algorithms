1707ee078e2510153105799ba0959aa9ce8b831a
CarefreeSlacker <struan.dirk.29@gmail.com>
2018-09-23 17:48:51 +0300

    Field module has been implemented #priority-queues

---
+++ lib/algorithms/contexts/priority_queue/field.ex
@@ -0,0 +1,119 @@
+defmodule Algorithms.PriorityQueue.Field do
+  @moduledoc """
+  Represent gaming field functions.
+  """
+
+  @empty_field 0
+
+  @type t :: %__MODULE__{}
+  defstruct standard_field: [], game_field: [], width: 3, height: 3
+
+  def initialize(width \\ 3, height \\ 3) do
+    field = initialize_standard_field(width, height)
+
+    %__MODULE__{
+      standard_field: field,
+      game_field: field,
+      width: width,
+      height: height
+    }
+  end
+
+  defp initialize_standard_field(width, height) do
+    1..height
+    |> Enum.reduce([], fn j, field ->
+      field_row =
+        1..width
+        |> Enum.reduce([], fn i, row ->
+          element =
+            cond do
+              i == width and j == height -> @empty_field
+              true -> i + (j - 1) * width
+            end
+
+          row ++ [element]
+        end)
+
+      field ++ [field_row]
+    end)
+  end
+
+  def swap(%__MODULE__{} = field, swapping) do
+    empty_tile = empty_tile_coordinate(field)
+
+    value = get_value(field, swapping)
+
+    field
+    |> set_value(empty_tile, value)
+    |> set_value(swapping, @empty_field)
+  end
+
+  defp get_value(%__MODULE__{game_field: game_field}, {x, y}) do
+    game_field
+    |> Enum.at(y)
+    |> Enum.at(x)
+  end
+
+  defp set_value(%__MODULE__{game_field: game_field} = field, {x, y}, value) do
+    new_game_field = game_field
+    |> List.update_at(y, fn row ->
+      row
+      |> List.update_at(x, fn _val -> value end)
+    end)
+    field
+    |> Map.put(:game_field, new_game_field)
+  end
+
+  def allowed_swap(%__MODULE__{width: w, height: h} = field_model) do
+    {x, y} = empty_tile_coordinate(field_model)
+
+    vertical_swap = cond do
+      y == 0 -> [{x, 1}]
+      y == (h - 1) -> [{x, y - 1}]
+      true -> [{x, y - 1}, {x, y + 1}]
+    end
+
+    horizontal_swap = cond do
+      x == 0 -> [{1, y}]
+      x == (w - 1) -> [{x - 1, y}]
+      true -> [{x - 1, y}, {x + 1, y}]
+    end
+
+    (vertical_swap ++ horizontal_swap)
+    |> Enum.filter(fn {x, y} ->
+      x >= 0 and y >= 0 and x < w and y < h
+    end)
+  end
+
+  def empty_tile_coordinate(%__MODULE__{game_field: field, width: width, height: height}) do
+    0..(height - 1)
+    |> Enum.reduce_while({}, fn j, result1 ->
+      0..(width - 1)
+      |> Enum.reduce_while({}, fn i, result2 ->
+        field
+        |> Enum.at(j)
+        |> Enum.at(i)
+        |> case do
+          @empty_field -> {:halt, {i, j}}
+          _value -> {:cont, result2}
+        end
+      end)
+      |> case do
+        {} -> {:cont, result1}
+        {x, y} -> {:halt, {x, y}}
+      end
+    end)
+  end
+
+  def shuffle(%__MODULE__{} = field, rate) when rate > 0 and is_integer(rate) do
+    allowed = allowed_swap(field)
+    swapping = Enum.at(allowed, :rand.uniform(length(allowed)) - 1)
+    field
+    |> swap(swapping)
+    |> shuffle(rate - 1)
+  end
+
+  def shuffle(%__MODULE__{} = field, 0) do
+    field
+  end
+end
---
+++ test/contexts/priority_queue/field_test.exs
@@ -0,0 +1,171 @@
+defmodule Algorithms.PriorityQueue.FieldTest do
+  use Algorithms.ConnCase
+
+  alias Algorithms.PriorityQueue.Field
+
+  describe "#initialize" do
+    test "Initialize 3x3 standard field if no paramteters given" do
+      assert %Field{
+               standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+               game_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+               width: 3,
+               height: 3
+             } = Field.initialize()
+    end
+
+    test "Initialize field with given attributes" do
+      assert %Field{
+               standard_field: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]],
+               game_field: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]],
+               width: 4,
+               height: 3
+             } = Field.initialize(4, 3)
+    end
+  end
+
+  describe "#swap" do
+    test "Get right swaps if empty point in corner #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        width: 3,
+        height: 3
+      }
+
+      assert [{2, 1}, {1, 2}] = Field.allowed_swap(field)
+
+      assert %{
+               game_field: [[1, 2, 3], [4, 5, 0], [7, 8, 6]]
+             } = Field.swap(field, {2, 1})
+
+      assert %{
+               game_field: [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
+             } = Field.swap(field, {1, 2})
+    end
+  end
+
+  describe "#allowed_swap" do
+    test "Get right swaps if empty point in corner #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        width: 3,
+        height: 3
+      }
+
+      assert [{2, 1}, {1, 2}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps if empty point in corner #2" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[0, 2, 3], [1, 5, 6], [4, 7, 8]],
+        width: 3,
+        height: 3
+      }
+
+      assert [{0, 1}, {1, 0}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps if empty point lie on edge #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[2, 0, 3], [1, 5, 6], [4, 7, 8]],
+        width: 3,
+        height: 3
+      }
+
+      assert [{1, 1}, {0, 0}, {2, 0}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps if empty point lie on edge #2" do
+      field = %Field{
+        standard_field: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]],
+        game_field: [[1, 2, 0, 4], [5, 6, 3, 8], [9, 10, 7, 11]],
+        width: 4,
+        height: 3
+      }
+
+      assert [{2, 1}, {1, 0}, {3, 0}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps if empty point in the middle of ield #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[2, 0, 3], [1, 5, 6], [4, 7, 8]],
+        width: 3,
+        height: 3
+      }
+
+      assert [{1, 1}, {0, 0}, {2, 0}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps if empty point in the middle of ield #2" do
+      field = %Field{
+        standard_field: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]],
+        game_field: [[1, 2, 3, 4], [5, 6, 0, 8], [9, 10, 7, 11]],
+        width: 4,
+        height: 3
+      }
+
+      assert [{2, 0}, {2, 2}, {1, 1}, {3, 1}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps even in one dimensional field #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3, 0]],
+        game_field: [[1, 2, 3, 0]],
+        width: 4,
+        height: 1
+      }
+
+      assert [{2, 0}] = Field.allowed_swap(field)
+    end
+
+    test "Get right swaps even in one dimensional field #2" do
+      field = %Field{
+        standard_field: [[1], [0], [2], [3]],
+        game_field: [[1], [0], [2], [3]],
+        width: 1,
+        height: 4
+      }
+
+      assert [{0, 0}, {0, 2}] = Field.allowed_swap(field)
+    end
+  end
+
+  describe "#empty_tile_coordinate" do
+    test "Return right coordinate #1" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        width: 3,
+        height: 3
+      }
+
+      assert {2, 2} = Field.empty_tile_coordinate(field)
+    end
+
+    test "Return right coordinate #2 use game field" do
+      field = %Field{
+        standard_field: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
+        game_field: [[1, 2, 3], [0, 5, 6], [4, 7, 8]],
+        width: 3,
+        height: 3
+      }
+
+      assert {0, 1} = Field.empty_tile_coordinate(field)
+    end
+
+    test "Return right coordinate #3 for not standard size" do
+      field = %Field{
+        standard_field: [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]],
+        game_field: [[1, 2, 0, 4], [5, 6, 3, 8], [9, 10, 7, 11]],
+        width: 3,
+        height: 3
+      }
+
+      assert {2, 0} = Field.empty_tile_coordinate(field)
+    end
+  end
+end
